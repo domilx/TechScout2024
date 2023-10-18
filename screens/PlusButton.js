dc3f0c4b615ada4b8d68767fae18659b7973bc16
Noril Werlen <noril.werlen@pm.me>
2023-10-18 13:35:13 -0400

    alert for + button

--- screens/PlusButton.js
+++ screens/PlusButton.js
@@ -1,66 +1,26 @@
-import React, { useState } from 'react';
-import { View, TouchableOpacity, Text, StyleSheet, Animated } from 'react-native';
+import React from 'react';
+import { View, TouchableOpacity, Text, StyleSheet, Alert } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 export default function FloatingButton() {
-    const [viewVisible, setViewVisible] = useState(false);
-    const rotation = useState(new Animated.Value(0))[0];
-    const scaleValue = useState(new Animated.Value(0))[0];
-
-    const spin = rotation.interpolate({
-        inputRange: [0, 1],
-        outputRange: ['0deg', '45deg']
-    });
-
-    const scale = scaleValue.interpolate({
-        inputRange: [0, 1],
-        outputRange: [0, 1]
-    });
-
-    const toggleView = () => {
-        if (viewVisible) {
-            setViewVisible(false);
-            Animated.parallel([
-                Animated.timing(rotation, {
-                    toValue: 0,
-                    duration: 200,
-                    useNativeDriver: true
-                }),
-                Animated.timing(scaleValue, {
-                    toValue: 0,
-                    duration: 200,
-                    useNativeDriver: true
-                })
-            ]).start();
-        } else {
-            setViewVisible(true);
-            Animated.parallel([
-                Animated.timing(rotation, {
-                    toValue: 1,
-                    duration: 200,
-                    useNativeDriver: true
-                }),
-                Animated.timing(scaleValue, {
-                    toValue: 1,
-                    duration: 200,
-                    useNativeDriver: true
-                })
-            ]).start();
-        }
-    };
+    const handleButtonPress = () => {
+        Alert.prompt(
+            'Enter Text',
+            'Please enter some text:',
+            (text) => {
+                if (text) {
+                    alert('You entered: ' + text);
+                }
+            }
+        );
+    }
 
     return (
-        <>
-            <Animated.View style={{ ...styles.expandingView, transform: [{ scale }] }}>
-                
-            </Animated.View>
-
-            <Animated.View style={{ ...styles.floatingButton, transform: [{ rotate: spin }] }}>
-                <TouchableOpacity onPress={toggleView}>
-                    <Icon name="add" size={30} color="#F6EB14" />
-                </TouchableOpacity>
-            </Animated.View>
-        </>
+        <View style={styles.floatingButton}>
+            <TouchableOpacity onPress={handleButtonPress}>
+                <Icon name="add" size={30} color="#F6EB14" />
+            </TouchableOpacity>
+        </View>
     );
 }
 
@@ -77,14 +37,4 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         zIndex: 1000
     },
-    expandingView: {
-        position: 'absolute',
-        right: 20,
-        bottom: 170,
-        backgroundColor: '#F8F8F8',
-        borderRadius: 20,
-        width: 350,
-        height: 550,
-        zIndex: 999,
-    }
 });
