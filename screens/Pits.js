4566d3525dfa7e14551a4706a2804a6084ecd098
RaphEth <102704362+RaphEth@users.noreply.github.com>
2023-11-11 21:15:33 -0500

    added empty input validation

--- screens/Pits.js
+++ screens/Pits.js
@@ -21,53 +21,134 @@ function Pits({ route }) {
     teamNumber: currentTeamNumber,  
   });
   
-  // save pit data to Async Storage
   
-const savePitData = async () => {
-  try {
-    const existingPitModels = await AsyncStorage.getItem('pitModels');
-    const pitModels = existingPitModels ? JSON.parse(existingPitModels) : [];
-
-    const existingModelIndex = pitModels.findIndex(
-      (model) => model.teamNumber === newPitData.teamNumber
-    );
-
-    if (existingModelIndex !== -1) {
-      // if model with the same teamNumber exists
-
-      // Handle replacing or rejecting data 
-      Alert.alert(
-        'Data Exists',
-        'A model with the same teamNumber already exists. Do you want to replace it with the new data?',
-        [
-          {
-            text: 'Cancel',
-            style: 'cancel',
-          },
-          {
-            text: 'Replace',
-            onPress: async () => {
-              // Replace existing data with the new one
-              pitModels[existingModelIndex] = newPitData;
-              await AsyncStorage.setItem('pitModels', JSON.stringify(pitModels));
-              Vibration.vibrate();
-            },
-          },
-        ],
-        { cancelable: false }
-      );
-    } else {
-      // Add the new Pit Model instance to the array
-      pitModels.push(newPitData);
-      // Return the new models to AsyncStorage
-      await AsyncStorage.setItem('pitModels', JSON.stringify(pitModels));
-      alert('Data saved to AsyncStorage');
-      Vibration.vibrate();
+  const savePitData = async () => {
+    try {
+      // Validation for empty data points
+      // Team Name
+      try {
+        if (!newPitData.teamName || newPitData.teamName.trim() === '') {
+          throw new Error('Team Name cannot be empty');
+        }
+      }catch (validationError) {
+        //console.error('Validation Error:', validationError.message);
+        Alert.alert('Missing Data', validationError.message);
+        return;
+      } 
+      // Robot Lenght
+      try {
+        if (!newPitData.RobotLength || newPitData.RobotLength.trim() === '') {
+          throw new Error('Robot Lenght cannot be empty');
+        }
+      }catch (validationError) {
+        //console.error('Validation Error:', validationError.message);
+        Alert.alert('Missing Data', validationError.message);
+        return;
+      }
+      // Robot Width
+      try {
+        if (!newPitData.RobotWidth || newPitData.RobotWidth.trim() === '') {
+          throw new Error('Robot Width cannot be empty');
+        }
+      }catch (validationError) {
+        //console.error('Validation Error:', validationError.message);
+        Alert.alert('Missing Data', validationError.message);
+        return;
+      }
+      // Robot Weight
+      try {
+        if (!newPitData.RobotWeight || newPitData.RobotWeight.trim() === '') {
+          throw new Error('Robot Weight cannot be empty');
+        }
+      }catch (validationError) {
+        //console.error('Validation Error:', validationError.message);
+        Alert.alert('Missing Data', validationError.message);
+        return;
+      }
+       // Robot Drive Type
+       try {
+        if (!newPitData.DriveType || newPitData.DriveType.trim() === '') {
+          throw new Error('Robot Drive Type cannot be empty');
+        }
+      }catch (validationError) {
+        //console.error('Validation Error:', validationError.message);
+        Alert.alert('Missing Data', validationError.message);
+        return;
+      }
+      // Robot Drive Motor Type
+      try {
+        if (!newPitData.DriveMotors || newPitData.DriveMotors.trim() === '') {
+          throw new Error('Robot Drive Type cannot be empty');
+          
+        }
+      }catch (validationError) {
+        //console.error('Validation Error:', validationError.message);
+        Alert.alert('Missing Data', validationError.message);
+        return;
+      }
+      // Robot Drive Motor Type
+      try {
+        if (!newPitData.DriverExperience || newPitData.DriverExperience.trim() === '') {
+          throw new Error('Driver Experice cannot be empty');
+        }
+      }catch (validationError) {
+        //console.error('Validation Error:', validationError.message);
+        Alert.alert('Missing Data', validationError.message);
+        return;
+      }
+      
+  
+      // Continue with the rest of the logic
+      try {
+        const existingPitModels = await AsyncStorage.getItem('pitModels');
+        const pitModels = existingPitModels ? JSON.parse(existingPitModels) : [];
+  
+        const existingModelIndex = pitModels.findIndex(
+          (model) => model.teamNumber === newPitData.teamNumber
+        );
+  
+        if (existingModelIndex !== -1) {
+          // if model with the same teamNumber exists
+  
+          // Handle replacing or rejecting data 
+          Alert.alert(
+            'Data Exists',
+            'A model with the same teamNumber already exists. Do you want to replace it with the new data?',
+            [
+              {
+                text: 'Cancel',
+                style: 'cancel',
+              },
+              {
+                text: 'Replace',
+                onPress: async () => {
+                  // Replace existing data with the new one
+                  pitModels[existingModelIndex] = newPitData;
+                  await AsyncStorage.setItem('pitModels', JSON.stringify(pitModels));
+                  Vibration.vibrate();
+                },
+              },
+            ],
+            { cancelable: false }
+          );
+        } else {
+          // Add the new Pit Model instance to the array
+          pitModels.push(newPitData);
+          // Return the new models to AsyncStorage
+          await AsyncStorage.setItem('pitModels', JSON.stringify(pitModels));
+          alert('Data saved to AsyncStorage');
+          Vibration.vibrate();
+        }
+      } catch (error) {
+        console.error('Error saving Pit Data:', error);
+      }
+    } catch (error) {
+      console.error('Error in the savePitData function:', error);
+      // Handle the error, you can show an alert or log it
+      Alert.alert('Error', 'An error occurred while processing the data.');
     }
-  } catch (error) {
-    console.error('Error saving Pit Data:', error);
-  }
-};
+  };
+  
  
 
   // dropdown selection rendering options 
