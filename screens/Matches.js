725e32c8be8412b10ac43bba57f291a0a1c70d72
RaphEth <102704362+RaphEth@users.noreply.github.com>
2023-12-13 16:54:36 -0500

    No chris match and UI revamps

--- screens/Matches.js
+++ screens/Matches.js
@@ -43,7 +43,7 @@ function Matches({ route }) {
        setMatchCount(await loadMatchCount(currentTeamNumber));
     };
     loadMatchDataOnMount();
-    console.log("chrismatch2222222 " + matchCount);
+    console.log("match count " + matchCount);
   },);
 
   const setField = (field, value) => {
