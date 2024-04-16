23f4c14cd508b3253f71b5382956fb8142cc6617
domilx <domenico2727@gmail.com>
2024-04-15 21:18:00 -0400

    added delete

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+rm -rf builds/
 timestamp=$(date +%Y%m%d%H%M%S)
 eas build -p android --profile preview --local --output=builds/TechScout_${timestamp}.apk
 eas build --platform ios --local --output=builds/TechScout_${timestamp}.ipa
